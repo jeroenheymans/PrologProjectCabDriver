8e7fd5bba5d821191f78a8f20b360c91c337e2ce
Jeroen Heymans <je.heymans@gmail.com>
2011-12-16 10:59:29 +0100

    Changed way of including other sourcefiles. Executing main/1 isn't required anymore and all functions are available in swipl after doing consult(main)

--- main.pl
+++ main.pl
@@ -1,15 +1,16 @@
 % Main program
 %
 % Contains a main-function to execute the program
-% Loads all the other functions
+% Loads all the other functions at first when compiling this file
 %
 % Execute in Prolog:
-%   ['program.pl'].
+%   consult(main)
 %   main(X).
 
+:-['city.pl'].
+:-['routeCalculation.pl'].
+:-['customer.pl'].
+:-['functions.pl'].
+
 main(P):-
-  ['city.pl'],
-  ['routeCalculation.pl'],
-  ['customer.pl'],
-  ['functions.pl'],
   distanceFromStart(3,P).
