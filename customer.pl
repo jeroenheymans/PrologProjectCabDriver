63b348b26f7c6297829eafdc1138177b0aa2c37b
Jeroen Heymans <je.heymans@gmail.com>
2011-12-16 11:26:37 +0100

    Added function to get all the customers that also need to go to the same destination as given customer. Could be used during following the path

--- customer.pl
+++ customer.pl
@@ -17,3 +17,15 @@ distanceFromStartToCustomer(CID,Distance):-
     customer(CID, _, _, CustomerStart, _),
     startNode(ParkingLot),
     minimumDistance(ParkingLot, CustomerStart, _, Distance).
+    
+% Get list of all the customers that are going to the same
+% destination as given customer.
+%   CID1 = Customer ID of given customer
+%   Customers = list of customers as result
+getCustomersSameDestination(CID1, Customers):-
+    customer(CID1,_,_,_,Destination),
+    findall(Customer,
+            ( customer(CID2,_,_,_,Destination),
+              \+CID1=CID2,
+              Customer=CID2),
+            Customers).
