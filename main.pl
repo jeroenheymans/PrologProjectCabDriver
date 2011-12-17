d047826c79400eb6fae178ae363129523ab52f52
Jeroen Heymans <je.heymans@gmail.com>
2011-12-17 17:36:43 +0100

    Build basis for looping over the customers, for the moment only outputting all the customers

--- main.pl
+++ main.pl
@@ -4,15 +4,19 @@
 % Loads all the other functions at first when compiling this file
 %
 % Execute in Prolog:
-%   consult(main)
-%   main(X).
+%   consult(main) or reconsult(main)
+%   main.
 
 :-['city.pl'].
 :-['routeCalculation.pl'].
 :-['customer.pl'].
 :-['functions.pl'].
 
-main(First):-
+main(_):-
     getDeparturesForPickupCustomers(CustomersToPickUp),
     keysort(CustomersToPickUp, CustomersToPickUpSorted),
-    nextCustomer(CustomersToPickUpSorted,First,_).
+    loop(CustomersToPickUpSorted).
+    
+loop(CustomersToPickUp):-
+    nextCustomer(CustomersToPickUp,First,_),
+    writeln(First).
