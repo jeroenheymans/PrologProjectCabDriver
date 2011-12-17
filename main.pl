467d461db5d6dba676c70f7ac8c90f96f9a4dbe5
Jeroen Heymans <je.heymans@gmail.com>
2011-12-17 17:51:58 +0100

    Added clock to main loop

--- main.pl
+++ main.pl
@@ -7,16 +7,32 @@
 %   consult(main) or reconsult(main)
 %   main.
 
+% Necessary includes
 :-['city.pl'].
 :-['routeCalculation.pl'].
 :-['customer.pl'].
 :-['functions.pl'].
 
+% Main function, needs to be executed for this program
 main(_):-
+    writeln('Calculating departure times for taxis'),
     getDeparturesForPickupCustomers(CustomersToPickUp),
     keysort(CustomersToPickUp, CustomersToPickUpSorted),
-    loop(CustomersToPickUpSorted).
+    writeln('Picking up customers'),
+    loop(0, CustomersToPickUpSorted).
     
-loop(CustomersToPickUp):-
-    nextCustomer(CustomersToPickUp,First,_),
-    writeln(First).
+% Ending of the main loop
+%   Clock = value of the internal clock
+loop(Clock, []):-
+    writeln('Finished calculations for all customers'),
+    writeln(Clock).
+    
+% Main loop
+%   Clock = value of the internal clock
+%   CustomersToPickUp = customers list still to be picked up
+%       Format: DepartureTimeTaxi-CustomerID
+loop(Clock, CustomersToPickUp):-
+    nextCustomer(CustomersToPickUp,FirstCustomer,CustomersToPickUpRest),
+    writeln(FirstCustomer),
+    NewClock is Clock + 1,
+    loop(NewClock, CustomersToPickUpRest).
