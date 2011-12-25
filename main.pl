85c43d37f86ec1fcf4ed9999b4bee6027d408f27
Jeroen Heymans <je.heymans@gmail.com>
2011-12-25 09:44:32 +0100

    Added debuginformation and put the clock in its own incrementClock/2 function

--- main.pl
+++ main.pl
@@ -8,8 +8,8 @@
 %   main.
 
 % Necessary includes
-:-['city_smaller.pl'].
-%:-['city.pl'].
+%:-['city_smaller.pl'].
+:-['city.pl'].
 :-['routeCalculation.pl'].
 :-['customer.pl'].
 :-['taxi.pl'].
@@ -20,35 +20,39 @@
 
 % Test function, used for testing small parts of code before
 % putting it in the rest of the code
+    %CustomersToPickUp = [1038-0,233-1,587-2],
 testmain:-
-    writeln('This is the testfunction').
+    getDeparturesForPickupCustomers(CustomersToPickUp),
+    keysort(CustomersToPickUp, CustomersToPickUpSorted),
+    writeln(CustomersToPickUpSorted).
 
 % Main function, needs to be executed for this program
 main(_):-
     getDeparturesForPickupCustomers(CustomersToPickUp),
-    %CustomersToPickUp = [1038-0,233-1,587-2],
     keysort(CustomersToPickUp, CustomersToPickUpSorted),
     assert(clock(0)),
     loop(0, CustomersToPickUpSorted).
     
-loop(3000, RemainingCustomers):-
+loop(1440, RemainingCustomers):-
     printTimesUp(RemainingCustomers).
           
 loop(Clock, []):-
-    retract(clock(Clock)),
-    NewClock is Clock + 1,
-    assert(clock(NewClock)),
+    incrementClock(Clock, NewClock),
     getTaxisInTransport(TaxisInTransport),
     moveTaxis(TaxisInTransport),
     loop(NewClock, []).
     
 loop(Clock, RemainingCustomers):-
-    retract(clock(Clock)),
-    NewClock is Clock + 1,
-    assert(clock(NewClock)),
+    incrementClock(Clock, NewClock),
     customersToPickupNow(NewClock, RemainingCustomers, CustomersNowToPickUp, CustomersToPickUpLater),
     printCustomersToPickUpNow(CustomersNowToPickUp),
     sendTaxisToCustomers(CustomersNowToPickUp),
     getTaxisInTransport(TaxisInTransport),
     moveTaxis(TaxisInTransport),
     loop(NewClock, CustomersToPickUpLater).
+    
+incrementClock(Clock, NewClock):-
+    retract(clock(Clock)),
+    NewClock is Clock + 1,
+    assert(clock(NewClock)).
+
