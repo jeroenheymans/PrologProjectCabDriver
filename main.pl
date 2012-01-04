63d176c4aebe927d9180f0344f4a5da4359a60eb
Jeroen Heymans <je.heymans@gmail.com>
2012-01-05 00:57:20 +0100

    Corected the backtracking problem

--- main.pl
+++ main.pl
@@ -33,21 +33,20 @@ loop([Taxi|Taxis]):-
 	retract(customer(Customer, ETOP, LTOP, StartID, DestID)),
 	startNode(Depot),
 	minimumDistance(Depot, StartID, Path, _), % check on minimumtime
-	loopInner([Customer], ETOP, InTaxi, Path),
+	loopInner([Customer], ETOP, InTaxi, Path, EndPath),
 	writeln(InTaxi),
-	writeln(Path),
+	writeln(EndPath),
 	loop(Taxis).
 	
-loopInner([C1,C2,C3,C4], _, [C1,C2,C3,C4], _).
+loopInner([C1,C2,C3,C4], _, [C1,C2,C3,C4], EndPath, EndPath).
 
-loopInner([FirstID|Customers], Time, InTaxi, [FStartID|TaxiPath]):-
+loopInner([FirstID|Customers], Time, InTaxi, [FStartID|TaxiPath], EndPath):-
 	customer(CustomerID, CETOP, CLTOP, CStartID, CDestID),
-	CustomerID =\= FirstID,
-	minimumDistance(FStartID, CStartID, Path, PathTime),
 	retract(customer(CustomerID, _, _, _, _)),
+	minimumDistance(FStartID, CStartID, Path, PathTime),
 	append([CustomerID], [FirstID|Customers], NewCustomers),
 	append(Path, [FStartID|TaxiPath], NewTaxiPath),
-	loopInner(NewCustomers, Time, InTaxi, NewTaxiPath).
+	loopInner(NewCustomers, Time, InTaxi, NewTaxiPath, EndPath).
 
-loopInner(Customers, _, InTaxi, _):-
+loopInner(Customers, _, InTaxi, EndPath, EndPath):-
 	InTaxi = Customers.
