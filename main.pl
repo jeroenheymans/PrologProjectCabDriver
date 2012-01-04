3fcbf26c7c02158b84c0fb229e912fbfcaab5b3d
Jeroen Heymans <je.heymans@gmail.com>
2012-01-04 21:01:09 +0100

    Cleaned up main.pl

--- main.pl
+++ main.pl
@@ -19,16 +19,6 @@
 :-['taxi.pl'].
 :-['functions.pl'].
 :-['print.pl'].
-
-test(CID1):-
-	customer(CID1, ETOP1, LTOP1, Start1, Dest1),
-	customer(CID2, ETOP2, LTOP2, Start2, Dest2),
-	CID1 =\= CID2,
-	minimumDistance(Start1, Start2, P, Time),
-	NewLTOP2 is LTOP2 - Time,
-	NewETOP2 is ETOP2 - Time,
-	(NewETOP2 =< LTOP1 , NewLTOP2 >= ETOP1),
-	write(CID1),write('&'),write(CID2),write(' ('),write(Time),write(') :'),write(ETOP1),write('-'),write(LTOP2),write('-'),write(NewETOP2),write('-'),writeln(NewLTOP2).
     
 main:-
 	getAllTaxis(Taxis),
@@ -39,20 +29,15 @@ loop([]):-
 
 loop([Taxi|Taxis]):-
 	retract(taxi(Taxi)),
-	write('Picked taxi '),writeln(Taxi),
 	retract(customer(Customer, ETOP, LTOP, StartID, DestID)),
 	loopInner([Customer], InTaxi),
-	write('In taxi: '),writeln(InTaxi),
-	%loopInner([Customer], Path, Customers, CustomersInTaxi),
-	%writeln(CustomersInTaxi).
 	loop(Taxis).
 	
 loopInner([C1,C2,C3,C4], [C1,C2,C3,C4]).
 
 loopInner(Customers, InTaxi):-
 	(customer(Customer, ETOP, LTOP, StartID, DestID)
-	-> (
-	retract(customer(Customer, ETOP, LTOP, StartID, DestID)),
-	append(Customers, [Customer], NewCustomers),
-	loopInner(NewCustomers, InTaxi))
+	-> (retract(customer(Customer, ETOP, LTOP, StartID, DestID)),
+		append(Customers, [Customer], NewCustomers),
+		loopInner(NewCustomers, InTaxi))
 	; InTaxi = Customers).
